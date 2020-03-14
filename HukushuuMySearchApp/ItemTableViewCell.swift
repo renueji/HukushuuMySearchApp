36c1e9887ae58db832f03d22b92e9537ba92150d
Ren <rentaroubiz@gmail.com>
2020-03-14 15:35:44 +0900

    Initial Commit

---
+++ HukushuuMySearchApp/ItemTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ItemTableViewCell.swift
+//  HukushuuMySearchApp
+//
+//  Created by Rentaro on 2020/03/08.
+//  Copyright © 2020 Rentaro. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemTitleView: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        // 元々入っている情報を再利用時にクリア
+        itemImageView.image = nil
+    }
+
+}
---
+++ HukushuuMySearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HukushuuMySearchApp
+//
+//  Created by Rentaro on 2020/03/08.
+//  Copyright © 2020 Rentaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
